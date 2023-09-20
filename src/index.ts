9660568a9e690a4adb6ceb54c517f9623789cf59
unknown <jhowjhoe@icloud.com>
2023-09-19 21:48:18 -0300

    update ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
 import axios from 'axios';
-
 class ApiMultas {
 
 	multas = async (state:string, host: string, token: string, data: Record<string, any>) => {
+
 		try {
 			
-			const request = await axios.request({
+			const response = await axios.request({
 				method: 'POST',
 				url: `${host}/multas/${state}`,
 				headers: {
-					'token': `${token}`,
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
 					'User-Agent': 'APIBRASIL/API-MULTAS',
+					'token': `${token}`,
 				},
 				data: data,
 			})
 
-			const response = await request;
-
 			return response.data;
 
 		} catch (e: any) {
-			
 			return e.response.data;
-
 		}
+
 	};
 
 };
